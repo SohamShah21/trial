c29539bab51eab8138507b313292158077e19ca8
SohamShah21 <soham.s.21@gmail.com>
2016-04-06 14:28:50 +0530

    Added files via upload

--- extractor.py
+++ extractor.py
@@ -5,6 +5,7 @@ Spyder Editor
 This temporary script file is located here:
 C:\Users\smdes\.spyder2\.temp.py
 """
+
 import math
 Qk = input("Kerosene flow rate in m3/hr: ")
 Qw = input("Water flow rate in m3/hr: ")
@@ -13,9 +14,9 @@ dw = 1000 # density of water
 nk = 0.00164
 nw = 0.001
 ift = 0.052
-Ce1 = input("concentration of EtSH at inlet ppm:")
-Cp1 = input("concnetraiton of PrSH at inlet ppm:")
-Cb1 = input("concnetraiton of BuSH at inlet ppm:")
+Ce1 = input("concentration of EtSH at inlet ppm: ")
+Cp1 = input("concentration of PrSH at inlet ppm: ")
+Cb1 = input("concentration of BuSH at inlet ppm: ")
 Cm_out = input("required concnetraiton of mercaptans at outlet ppm:")
 
 dp = 1.15*((ift/((dw-dk)*9.81))**0.5)
@@ -43,7 +44,7 @@ v = Qk/Qw
 #Re_disp = dw*Vd*dp/nw
 #Cd = (24/Re_disp)
 Vk = 0.33*((dw-dk)**0.4)*(dw**-0.6)*(dp**-0.8)*(nk**0.2)*(vdfr/ap)    
-phi = 0.1
+phi = 0.5
 Vso = (dw-dk)*dp*dp*9.81/(18*nk)
 Vdf = 0.1*Vso
 Vcf = 0.2*Vso
@@ -66,7 +67,7 @@ print "error in hold up = ", error
 '''
 Calculation of diameter from known flooding velocity
 '''
-D = (((Qk+Qw)*4)/0.6*(Vcf+Vdf)*math.pi)**0.5 ## taking actual velocity as 0.6 of flooding velocity
+D =(((Qk+Qw)*4)/0.6*(Vcf+Vdf)*math.pi)**0.5 ## taking actual velocity as 0.6 of flooding velocity
 Vc = Qk/(math.pi*(D**2)*0.25*3600)
 Vd = Qw/(math.pi*(D**2)*0.25*3600)
 Re_disp = dw*Vd*dp/nw
@@ -78,7 +79,7 @@ print "Diameter of Column = ", D
 Calculation of mass transfer coefficients
 '''
 Aet = 4.23
-Apr = 7.75
+Apr = 7.756
 Abu = 17.58
 A1 = (0.95+c)**4.119
 A2 = (ap/vdfr)**0.091
@@ -86,7 +87,7 @@ A3 = (100*Vd)**0.837
 Kod_e = Aet*A1*A2*A3
 Kod_b = Abu*A1*A2*A3
 Kod_p = Apr*A1*A2*A3
-print Kod_e
+# print Kod_e
 ye = 0
 yp = 0
 yb = 0
@@ -94,11 +95,16 @@ Ce = Ce1
 Cp = Cp1
 Cb = Cb1
 h=0
-dh = 1
+dh = 0.5
 while Ce+Cp+Cb > Cm_out:
-    
+    if Ce < 0:
+        Ce = 0
+    if Cp < 0:
+        Cp = 0
+    if Cb < 0:
+        Cb = 0
     dne = Kod_e*(Pet*Ce)*Ac*dh*3600*(10**-6)
-    dnp = Kod_p*(Ppr*Cb)*Ac*dh*3600*(10**-6)
+    dnp = Kod_p*(Ppr*Cp)*Ac*dh*3600*(10**-6)
     dnb = Kod_b*(Pbu*Cb)*Ac*dh*3600*(10**-6)
     Ce = Ce - dne/Qk
     Cp = Cp - dnp/Qk
@@ -106,14 +112,12 @@ while Ce+Cp+Cb > Cm_out:
     ye = ye+(dne/Qw)
     yp = yp + (dnp/Qw)
     yb = yb + (dnb/Qw)
+    c= c- (dne+dnp+dnb)/(1000000*Qw/40)
     h = h+dh
-    if Ce < 0:
-        Ce = 0
-    if Cp < 0:
-        Cp = 0
-    if Cb < 0:
-        Cb = 0
+    
+
 print "Ce = ",Ce
 print "Cp = ",Cp
 print "Cb = ",Cb
 print "h = ",h
+print "c = ",c
